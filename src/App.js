e035124d5a8067c9bec1eac4d651736a2542e707
Anant Kumawat <anantkumawat22@gmail.com>
2022-06-01 15:38:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 
 function App() {
 
+  const [alert, setalert] = useState('');
+  const [chgcss, setchgcss] = useState('none');
   const [data, setdata] = useState('');
   const [theme, settheme] = useState('light');
 
@@ -18,34 +20,46 @@ function App() {
     }
   }
 
+  const alertfun = (mssg) => {
+    setchgcss("block");
+    setalert(mssg);
+    setTimeout(() => {
+      setchgcss("none");
+    }, 2000);
+  }
+
   const changedata = (e) => {
     setdata(e.target.value);
   }
   const conuppercase = () => {
     setdata(data.toUpperCase());
-
+    alertfun("Your Text is successfully converted into Uppercase.");
   }
 
   const conlowercase = () => {
     setdata(data.toLowerCase());
+    alertfun("Your Text is successfully converted into Lowercase.");
   }
 
   const cleardata = () => {
     setdata('');
+    alertfun("Text Cleared.");
   }
 
   const copydata = () => {
     navigator.clipboard.writeText(data);
+    alertfun("Copied to Clipboard.");
   }
 
   const removespaces = () => {
     setdata(data.replace(/\s+/g, ' ').trim());
+    alertfun("Extra spaces has been removed successfully.");
   }
 
   return (
     <>
       <Navbar theme={theme} toggletheme={toggletheme} />
-      {/* <div class="alert alert-success" role="alert"></div> */}
+      <div className={`alert alert-success d-${chgcss}`} role="alert">{alert}</div>
       <div className="container d-flex flex-column justify-content-center">
         <h2 className={`pt-3 text-${theme === 'light' ? 'dark' : 'light'}`}>Enter Some Text Here</h2>
         <textarea value={data} onChange={changedata} className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
